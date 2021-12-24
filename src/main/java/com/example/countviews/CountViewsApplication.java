4ca86415cab8d312c6be60e30b8da6ac18bb4f74
quanghuy2002 <hoangqunghuy753@gmail.com>
2021-12-24 15:55:24 +0700

    th bài 10

---
+++ src/main/java/com/example/countviews/CountViewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.countviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountViewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountViewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/countviews/CountViewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountViewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
